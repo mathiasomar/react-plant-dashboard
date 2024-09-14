440689137d99e2eb5b519a61780d67d41c276969
mathiasomar <ommykmathy@gmail.com>
2024-09-14 16:01:13 +0300

    creating the layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import { Layout } from "antd";
+import Sidebar from "./components/Sidebar";
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const { Sider, Header, Content } = Layout;
 
+function App() {
+  const [collapsed, setCollapsed] = useState(false);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Layout>
+      <Sider
+        theme="light"
+        trigger={null}
+        collapsible
+        collapsed={collapsed}
+        className="sider"
+      >
+        <Sidebar />
+      </Sider>
+      <Layout>
+        <Header className="header"></Header>
+        <Content className="content"></Content>
+      </Layout>
+    </Layout>
+  );
 }
 
-export default App
+export default App;
